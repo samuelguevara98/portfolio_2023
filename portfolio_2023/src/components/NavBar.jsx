d7dacfecc4e12cfe458ee3dcdca154b03af2cf7d
Samuel Guevara <samsjamin@outlook.com>
2023-07-11 00:46:35 -0400

    added class mdhidden to hide the hamburger menu from appearing on large displays

--- portfolio_2023/src/components/NavBar.jsx
+++ portfolio_2023/src/components/NavBar.jsx
@@ -43,14 +43,14 @@ const NavBar = () => {
 
         <div 
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-400">
+        className="cursor-pointer pr-4 z-10 text-gray-400 md:hidden">
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
 
 
         {nav && (
           
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-600">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-600 ">
         {links.map(({ id, link }) => (
           
           <li 
