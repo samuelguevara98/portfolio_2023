93f55c2d796ad77c133f5e3f5bcbbf961c35d95d
Samuel Guevara <samsjamin@outlook.com>
2023-07-12 23:15:08 -0400

    Created a social link component that is responsive. It has my resume, which does not download it, it opens it in another tab to view, which gives the user a chance to download it if they wish. I used all filled icons for continuancy, the social links compenent is set to be hidden until an xl screen views it, so it does not block any text.

--- portfolio_2023/src/App.js
+++ portfolio_2023/src/App.js
@@ -1,5 +1,5 @@
 import Home from "./components/Home";
-import NavBar from "./components/NavBar" 
+import NavBar from "./components/NavBar";
 //import About from "./components/About"; import Portfolio from './components/Portfolio' import Experience from "./components/Experience" import Contact from "./components/Contact" 
 import SocialLinks from "./components/SocialLinks"
 
